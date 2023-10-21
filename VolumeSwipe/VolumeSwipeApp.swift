5eac45cc3d27ab9e6e083a2623e35406ecf5599c
Dmitry <d.myachn@gmail.com>
2023-10-21 13:18:54 +0300

    Initial Commit

---
+++ VolumeSwipe/VolumeSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeSwipeApp.swift
+//  VolumeSwipe
+//
+//  Created by Dmitiy Myachin on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct VolumeSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
